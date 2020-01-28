a938994efb8bbb9081e576225a0f3ce92dcb22f6
tatsuro13 <oscarwild13@gmail.com>
2020-01-28 19:00:45 +0900

    60:puppeteer実装

---
+++ functions/src/crawlers/kodansha-calendar.ts
@@ -0,0 +1,30 @@
+import puppeteer from 'puppeteer';
+import { FeedMemo, blankFeedMemo } from '../services/comic_search/models/feed-memo';
+
+export const feedCalender = async (page: puppeteer.Page) => {
+  const url = 'http://kc.kodansha.co.jp/calendar';
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+  const memos: FeedMemo[] = [];
+
+  for await (const _ of [0, 1]) {
+    const items = await page.$$('.comicList .contIn .contR .spaceSp .block');
+
+    for await (const item of items) {
+      const memo = { ...blankFeedMemo };
+
+      const releaseDate = await item.$eval('.date', e => { e.textContent });
+      if (releaseDate) memo.releaseDate = releaseDate.replace(/\s/g, '-');
+      memo.title = await item.$eval('.tit', e => { e.textContent });
+      const author = await item.$eval('.name', e => { e.textContent });
+      if (author) memo.author = author.replace(/\s/g, ' ').replace(/, /g, ',');
+      memo.publisher = 'kodansha';
+
+      memos.push(memo);
+    }
+
+    await page.click('.linkTitW > ul > li:nth-child(2)');
+  }
+
+  return memos;
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,23 @@
 import * as functions from 'firebase-functions';
 import admin from 'firebase-admin';
+import puppeteer from 'puppeteer';
+
 import { collectionName } from './services/comic_search/constants';
+import { feedCalender } from './crawlers/kodansha-calender';
+import { saveFeedMemo } from './firestore-admin/feed-memo';
+
+const PUPPETEER_OPTIONS = {
+  args: [
+    '--disable-gpu',
+    '--disable-dev-shm-usage',
+    '--disable-sandbox',
+    '--no-first-run',
+    '--no-first-sandbox',
+    '--no-first-zygote',
+    '--single-process',
+  ],
+  headless: true,
+};
 
 admin.initializeApp();
 
@@ -13,4 +30,23 @@ export const publishers = functions
       .get();
     const data = snap.docs.map(doc => doc.data());
     res.send({ data });
+  });
+
+export const feedCalender = functions
+  .region('asia-northeast1')
+  .runWith({
+    timeoutSeconds: 300,
+    memory: '2GB',
+  })
+  .pubsub.schedule('0 2 1, 10,20 * *')
+  .onRun(async () => {
+    const browser = await puppeteer.launch(PUPPETEER_OPTIONS);
+    const page = await browser.newPage();
+    const db = admin.firestore();
+
+    const memos = await feedCalender(page);
+    const fetchCount = await saveFeedMemo(db, memos, 'kodansha');
+
+    await browser.close();
+    console.log(`Fetched Kodansha calendar. Wrote ${fetchCount} memos.`);
   });
